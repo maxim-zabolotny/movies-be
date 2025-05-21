7ba518795f4be8aac1ea31feda1d5a14269fb7fb
owerlord1 <maxim.zabolotny20121776@gmail.com>
2025-05-21 03:19:47 +0300

    add auth middleware

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { MovieController } from '../controllers/movie.controller';
 import { upload } from '../config/multer.config';
+import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = Router();
 const movieController = new MovieController();
 
-router.post('/', movieController.create);
-router.get('/:id', movieController.getById);
-router.patch('/:id', movieController.update);
-router.delete('/:id', movieController.delete);
-router.get('/', movieController.getAll);
-
-router.post('/import', upload.single('movies'), movieController.importMovies);
+router.post('/', authMiddleware, movieController.create);
+router.get('/:id', authMiddleware, movieController.getById);
+router.patch('/:id', authMiddleware, movieController.update);
+router.delete('/:id', authMiddleware, movieController.delete);
+router.get('/', authMiddleware, movieController.getAll);
+router.post('/import', authMiddleware, upload.single('movies'), movieController.importMovies);
 
 export default router;
